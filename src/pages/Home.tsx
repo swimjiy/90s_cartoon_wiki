bf9089e5727b91f7fd1bca2cf359aba77648ab96
swimjiy <swimjiy@gmail.com>
2020-08-26 23:50:53 +0900

    Fix lint error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 
-const Home: React.FC = () => {
-    return (
-        <div>
-            <Grid container>
-                <Grid item xs={12} sm={6}>
-                    안녕
-                </Grid>
-            </Grid>
-        </div>
-    )
-}
+const Home: React.FC = () => (
+  <div>
+    <Grid container>
+      <Grid item xs={12} sm={6}>
+        안녕
+      </Grid>
+    </Grid>
+  </div>
+);
 export default Home;
